b0b9a9e3a4b2748112db845342a5dd45160ffe78
mcamack <mcamack@gmail.com>
2018-04-01 10:35:14 -0700

    add Objects dir

---
+++ Objects/script.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var cat = Object.create(Object.prototype,
+    {
+        name: {
+            value: "Fluffy",
+            enumerable: true,
+            writable: true,
+            configurable: true
+        },
+
+        color: {
+            value: "White",
+            enumerable: true,
+            writable: true,
+            configurable: true
+        }
+    }
+)
+
+display(cat)
